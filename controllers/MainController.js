516171ed59dbeb54d0516b0a4382654051356b8e
Playmaker8 <playmaker8@ukr.net>
2016-05-12 22:26:33 +0300

    Create project structure

---
+++ controllers/MainController.js
@@ -0,0 +1,12 @@
+;(function () { 
+"use strict"; 
+
+var app = angular.module("app");
+
+app.controller("MainController", MainController); 
+
+function MainController($scope, $http) { 
+var vm = this;
+}
+
+})();
